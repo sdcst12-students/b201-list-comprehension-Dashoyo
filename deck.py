5fa33725fab731cd215c52549d4967c9bb3e4777
Dashoyo <113055239+Dashoyo@users.noreply.github.com>
2023-09-19 12:29:54 -0700

    Done

--- deck.py
+++ deck.py
@@ -1,5 +1,22 @@
 #!python3
-
+import random
+ask = int(input("How many cards do you want?"))
 ranks = ['A','2','3','4','5','6','7','8','9','T','J','Q','K']
 suits = ['C','D','H','S']
+
+deck = [f"{i}{j}"for i in ranks for j in suits]
+
+for i in deck:
+
+    def shuffle():
+        random.shuffle(deck)
+        return shuffle
+    def draws():
+        ask = int(input("How many cards do you want?"))
+        draw = []
+        for i in range(ask):
+            draw.append(deck[i])
+            deck.pop(i)
+            
+        return draw
 
